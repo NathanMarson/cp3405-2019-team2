452be8cbfda86154cf3d13f8a38f20aad797a074
PeterMenzel <peter.menzel@my.jcu.edu.au>
2019-09-29 14:42:49 +1000

    Add fragment_login and implement.

---
+++ JobLink/app/src/main/java/com/cp3405/joblink/ui/login/LoginFragment.java
@@ -0,0 +1,35 @@
+package com.cp3405.joblink.ui.login;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.cp3405.joblink.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+public class LoginFragment extends Fragment {
+
+    private LoginViewModel loginViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        loginViewModel =
+                ViewModelProviders.of(this).get(LoginViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_login, container, false);
+        final TextView textView = root.findViewById(R.id.text_login);
+        loginViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
